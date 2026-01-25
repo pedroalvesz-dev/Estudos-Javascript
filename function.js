aeeed18fee56ec5c4a79d5c437f563dc6a7de4e8
Pedro Alves <pedroalvesz-dev@users.noreply.github.com>
2026-01-25 18:09:38 -0300

    feat: aprendendo funções

---
+++ function.js
@@ -0,0 +1,36 @@
+torrar()
+PaoFritando()
+
+function torrar(){
+    console.log("pao esta sendo torrado")
+}
+function PaoFritando(){
+        console.log("seu pao esta sendo fritado ")
+        console.log("finalizando")
+}
+
+checkpoint()
+function checkpoint(){
+    saveGame()
+    exitGame()  
+}
+function saveGame(){
+    console.log("dados do jogo salvo")
+}
+function exitGame(){
+        console.log("jogo salvo e fechado")
+        console.log("finalizado jogo")
+
+}
+EnviarDados()
+function EnviarDados(){
+    let nomeDoBanco="nubank"
+    if(3===3){
+        console.log(" senha valida: " + " seja bem vindo equipe: "+nomeDoBanco)
+    }
+    else
+            console.log("senha invalida")
+}
+        
+
+
