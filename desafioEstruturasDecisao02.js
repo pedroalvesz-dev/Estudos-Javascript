f0f356eeb3bdf7d36f129a8a07ea1950e5495700
Pedro Alves <pedroalvesz-dev@users.noreply.github.com>
2026-01-24 21:04:36 -0300

    feat: Entendendo Estrutura de Decisão e Repetição de JavaScript com a Blip

---
+++ desafioEstruturasDecisao02.js
@@ -0,0 +1,20 @@
+const comando = gets();
+
+switch(comando){
+    case "horario":
+    print("Atendemos das 8h as 18h") 
+    break
+    
+    case"telefone":
+    print("Nosso telefone e 4002-8922")
+    break
+
+    case"servicos":
+    print("Oferecemos suporte, vendas e consultoria")
+    break
+
+    default:
+    print("comando invalido")
+    break
+
+}
